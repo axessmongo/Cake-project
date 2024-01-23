7add7b8d299b4ccbabb8d77f464837176a1b0b20
Jagan <jagannathan880@gmail.com>
2024-01-23 13:02:40 +0530

    new

--- cake/src/Component/Main.jsx
+++ cake/src/Component/Main.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import Banner from './Banner'
-import Celebrate from './Celebrate'
-import Categories from './Categories'
-import Occasions from './Occasions'
-import Unique from './Unique'
-import Trending from './Trending'
-import Bestselling from './Bestselling'
-import Sending from './Sending'
-import Footer from './Footer'
+
 
 function Main() {
   return (
